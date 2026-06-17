0ee6061b742b559efdddf1cc8479ad0c86cb9280
Giuseppe Di Mauro <azrafe7@gmail.com>
2026-06-17 17:04:13 +0200

    add shuffle (not used for now)

--- projects/script.js
+++ projects/script.js
@@ -1,4 +1,25 @@
 (function () {
+
+  // https://bost.ocks.org/mike/shuffle/
+  // https://en.wikipedia.org/wiki/Fisher%E2%80%93Yates_shuffle
+  function shuffle(array) {
+    var m = array.length, t, i;
+
+    // While there remain elements to shuffle…
+    while (m) {
+
+      // Pick a remaining element…
+      i = Math.floor(Math.random() * m--);
+
+      // And swap it with the current element.
+      t = array[m];
+      array[m] = array[i];
+      array[i] = t;
+    }
+
+    return array;
+  }
+  
   const grid = document.getElementById('grid');
 
   function render(projects) {
@@ -27,4 +48,6 @@
   }
 
   render(PROJECTS);
+  //render(shuffle(PROJECTS));
+
 })();
