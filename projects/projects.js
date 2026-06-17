bd274ca4af71214a43c7377c8b1fde284df4a7d0
Giuseppe Di Mauro <azrafe7@gmail.com>
2026-06-17 16:50:15 +0200

    more projects

--- projects/projects.js
+++ projects/projects.js
@@ -40,5 +40,77 @@ const PROJECTS = [
     description: "Delaunay triangulations, Voronoi, convex hull and more. Ported to Haxe 3 from https://github.com/sledorze/hxDelaunay (itself a port of the excellent https://github.com/nodename/as3delaunay).",
     image: "assets/hxDelaunay.png",
     url: "https://htmlpreview.github.io/?https://raw.githubusercontent.com/azrafe7/hxDelaunay/refs/heads/master/bin/html5/bin/index.html"
-  }
+  },
+  {
+    name: "hxClipper",
+    description: "Port of Angus Johnson's Clipper lib v6.4.2 to haxe 3.1+",
+    image: "assets/hxClipper.png",
+    url: "https://htmlpreview.github.io/?https://raw.githubusercontent.com/azrafe7/hxClipper/refs/heads/master/bin/js/index.html"
+  },
+  {
+    name: "hxDaedalus",
+    description: "Haxe cross target version of the as3 Daedalus-lib (https://code.google.com/p/daedalus-lib/) co-authored with Justin Mills (@nanjizal).",
+    image: "assets/hxDaedalus.png",
+    url: "https://htmlpreview.github.io/?https://github.com/hxDaedalus/hxDaedalus-Examples/blob/master/hxDaedalus-Examples/web/DaedalusBitmapPathfinding.html"
+  },
+  {
+    name: "Excel to PDF mail-merge",
+    description: "Use entries from a Google/Excel spreadsheet to fill and download multiple PDFs, via API or CLI.",
+    image: "assets/excel_mail_merge_to_pdf.png",
+    url: "https://www.upwork.com/freelancers/~01a0c41e84b3f00ea8?p=1843463777428619264"
+  },
+  {
+    name: "MidJourney Feed Downloader",
+    description: "Extract and save images from MidJourney showcase feed.",
+    image: "assets/midjourney_feed_downloader.png",
+    url: "https://www.youtube.com/watch?v=yUDKaDZFUig"
+  },
+  {
+    name: "Send 2 Photopea (Chrome Extension)",
+    description: "Send images and videos right to Photopea with one-click.",
+    image: "assets/send_2_photopea.png",
+    url: "https://chromewebstore.google.com/detail/send-2-photopea/ngjdphpkaopdenfbgkkkgmajgcmhpkme"
+  },
+  {
+    name: "Element Zapper (Chrome Extension)",
+    description: "Zap/remove any element. Get rid of annoying things on the internet.",
+    image: "assets/Element Zapper.png",
+    url: "https://chromewebstore.google.com/detail/element-zapper/noopjpinkmgojgljobbpbiehkhmaopio"
+  },
+  {
+    name: "Pop Element (Chrome Extension)",
+    description: "Pop out the current page (or an element) into a separate window.",
+    image: "assets/Pop Element.png",
+    url: "https://chromewebstore.google.com/detail/popelement/cbkjfkgogipoonckgoppiofacaocpfdj"
+  },
+  {
+    name: "Black & White Web (Chrome Extension)",
+    description: "Browse the Web in Black & White.",
+    image: "assets/B&W Web.png",
+    url: "https://chromewebstore.google.com/detail/black-white-web/eeghanoecgfjjflojdbjhmdpcdjceldh"
+  },
+  {
+    name: "Masterin Scraper",
+    description: "Scrape masterin site with Python",
+    image: "assets/Masterin Scraper.png",
+    url: "https://www.youtube.com/watch?v=vxorDz8i6l4"
+  },
+  {
+    name: "Maya scripts",
+    description: "Handy scripts to speed up Maya workflow.",
+    image: "assets/maya_scripts.png",
+    url: "https://www.youtube.com/watch?v=oWW7kGEasiQ"
+  },
+  {
+    name: "VLC 4 YouTube (Firefox Extension)",
+    description: "Click the extension icon when you are on a YouTube™ page to open the current video in VLC (or just hit Alt+V).",
+    image: "assets/vlc_4_yotube.jpg",
+    url: "https://addons.mozilla.org/it/firefox/addon/vlc-4-youtube-beta-4-firefox/"
+  },
+  {
+    name: "template",
+    description: "short desc.",
+    image: "assets/image.png",
+    url: "https://azrafe7.github.io/"
+  },
 ];
